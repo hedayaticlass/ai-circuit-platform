a27f85075a3d209c383a6046c6481acf5a9bca45
mhmdtdlt78 <mohammadazandaryani2005@gmail.com>
2025-12-22 16:26:23 +0330

    update setting.py

--- as1/as1/settings.py
+++ as1/as1/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'as1.home',
+    'home',
     'django_jalali',
     'django_extensions',
     'rest_framework',
